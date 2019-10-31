29abf41736611503c7ccfc565410065e2b5808d0
Ken Day <hello@ken-day.co.uk>
2019-10-30 21:43:45 -0700

    add image assets and finish about page

--- pages/assets/js/main.js
+++ pages/assets/js/main.js
@@ -17,11 +17,39 @@ function checkComponents() {
     // call navbar function
     
     hideNavbarOnScroll();
+    initOwlCarousels();
   }
 }
 
 // Hide nav on scroll down and show on scroll up
+
 function hideNavbarOnScroll() {
   
   $(".navbar.navbar-expand-lg.navbar-light").autoHidingNavbar();
+}
+
+// Project carousels 
+
+function initOwlCarousels() {
+
+  $('.owl-carousel').owlCarousel({
+    margin: 20,
+    loop: false,
+    items: 4,
+    dots: true,
+    responsive:{
+      320: {
+          items:2
+      },
+      600: {
+          items:2
+      },
+      1000: {
+          items:3
+      },
+      1600: {
+          items:4
+      }
+    }
+  })
 }
